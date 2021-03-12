df0e7caf138a6102b0c655e281d6599fd7fccf5a
Plagiatus <lukas.scheuerle@web.de>
2021-03-12 22:27:56 +0100

    updated and improved website

--- website/generator.js
+++ website/generator.js
@@ -3,29 +3,64 @@ const fs = require('fs');
 
 let index = fs.readFileSync('index.html').toString();
 
-let headdrops = marked(fs.readFileSync('../headdrops/README.md', 'utf-8'));
-let homes = marked(fs.readFileSync('../homes/README.md', 'utf-8'));
-let itemchallenge = marked(fs.readFileSync('../itemchallenge/README.md', 'utf-8'));
-let mobchallenge = marked(fs.readFileSync('../mobchallenge/README.md', 'utf-8'));
-let multiplayer_sleep = marked(fs.readFileSync('../multiplayer_sleep/README.md', 'utf-8'));
-let preload_world = marked(fs.readFileSync('../preload_world/README.md', 'utf-8'));
-let super_sponge = marked(fs.readFileSync('../super_sponge/README.md', 'utf-8'));
-let hardcore_realm = marked(fs.readFileSync('../hardcore_realm/README.md', 'utf-8'));
-let timer = marked(fs.readFileSync('../timer/README.md', 'utf-8'));
 
-let utility = [headdrops, homes, multiplayer_sleep, preload_world, super_sponge, hardcore_realm, timer];
-let games = [itemchallenge, mobchallenge];
+let utility = [
+    {
+        content: marked(fs.readFileSync('../headdrops/README.md', 'utf-8')),
+        id: "headdrops"
+    },
+    {
+        content: marked(fs.readFileSync('../homes/README.md', 'utf-8')),
+        id: "homes"
+    },
+    {
+        content: marked(fs.readFileSync('../multiplayer_sleep/README.md', 'utf-8')),
+        id: "multiplayer_sleep"
+    },
+    {
+        content: marked(fs.readFileSync('../preload_world/README.md', 'utf-8')),
+        id: "preload_world"
+    },
+    {
+        content: marked(fs.readFileSync('../super_sponge/README.md', 'utf-8')),
+        id: "super_sponge"
+    },
+    {
+        content: marked(fs.readFileSync('../hardcore_realm/README.md', 'utf-8')),
+        id: "hardcore_realm"
+    },
+    {
+        content: marked(fs.readFileSync('../timer/README.md', 'utf-8')),
+        id: "timer"
+    },
+];
+
+let games = [
+    {
+        content: marked(fs.readFileSync('../itemchallenge/README.md', 'utf-8')),
+        id: "itemchallenge"
+    },
+    {
+        content: marked(fs.readFileSync('../mobchallenge/README.md', 'utf-8')),
+        id: "mobchallenge"
+    },
+];
 
 let allUtility = "";
+let allContent = "";
 for(let u of utility){
-    allUtility += `<div class="pack">${u}</div>`;
+    allUtility += `<div class="pack" id="${u.id}">${u.content.match(/<img([^>]*[^/])>/)[0]}</div>`;
+    allContent += `<div class="pack-big" id="${u.id}">${u.content}</div>`;
 }
 index = index.replace('UTILITY', allUtility.replace(/<a/g,"<a target='_blank'"));
 
 let allGames = "";
 for(let g of games){
-    allGames += `<div class="pack">${g}</div>`;
+    allGames += `<div class="pack" id="${g.id}">${g.content.match(/<img([^>]*[^/])>/)[0]}</div>`;
+    allContent += `<div class="pack-big" id="${g.id}">${g.content}</div>`;
 }
 index = index.replace('GAMES', allGames.replace(/<a/g,"<a target='_blank'"));
+
+index = index.replace('ALLPACKS', allContent.replace(/<a/g,"<a target='_blank'"));
 
 fs.writeFileSync('../index.html', index);
--- website/script.js
+++ website/script.js
@@ -1,20 +1,30 @@
 window.addEventListener("load", init);
 
 let packs = []
+let bigPacks = []
 function init() {
     packs = document.getElementsByClassName("pack");
+    bigPacks = document.getElementsByClassName("pack-big");
     for(let pack of packs){
-        pack.addEventListener("click", toggleEnlarge);
+        pack.addEventListener("click", enlarge);
+    }
+
+    document.getElementById("close-btn").addEventListener("click", closePopup)
+}
+
+function enlarge(e){
+    // if(e.currentTarget != e.target) return;
+
+    document.getElementById("pack-popup").classList.add("show");
+    for(let pack of bigPacks){
+        if(pack.id != e.currentTarget.id) {
+            pack.classList.remove("show");
+        } else {
+            pack.classList.add("show");
+        }
     }
 }
 
-function toggleEnlarge(e){
-    if(e.target.tagName == "PRE") return;
-    if(e.target.tagName == "CODE") return;
-    if(e.target.tagName == "A") return;
-    e.currentTarget.classList.toggle("enlarged");
-    for(let pack of packs){
-        if(pack == e.currentTarget) continue;
-        pack.classList.remove("enlarged");
-    }
+function closePopup() {
+    document.getElementById("pack-popup").classList.remove("show");
 }
